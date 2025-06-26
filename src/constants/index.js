9794e2cf4222caeb533af41aa058c99e3c9f2fe3
Woolf <woolfson1995@gmail.com>
2025-06-26 13:02:11 +0300

    add basic settings

---
+++ src/constants/index.js
@@ -0,0 +1,27 @@
+import path from 'node:path';
+
+export const RECIPE_SORT_KEYS = [
+  '_id',
+  'name',
+  //допишете остальное
+];
+
+export const SORT_ORDER = {
+  ASC: 'asc',
+  DESC: 'desc',
+};
+
+export const FIFTEEN_MINUTES = 15 * 60 * 1000;
+export const ONE_DAY = 24 * 60 * 60 * 1000;
+
+export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
+
+export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
+
+export const CLOUDINARY = {
+  CLOUD_NAME: 'CLOUD_NAME',
+  API_KEY: 'API_KEY',
+  API_SECRET: 'API_SECRET',
+};
+
+export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
--- src/server.js
+++ src/server.js
@@ -1,17 +1,18 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 import { getEnvVar } from './utils/getEnvVar.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
-import mongoose from 'mongoose';
-
+import router from './routers/index.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
 export const setupServer = () => {
   const app = express();
-  app.use(express.json());
   app.use(cors());
   app.use(
     pino({
@@ -20,49 +21,14 @@ export const setupServer = () => {
       },
     }),
   );
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
 
-    res.status(200).json({
-      status: 200,
-      message: "Successfully found contacts!",
-      data: contacts,
-    });
-  });
+  app.use('/api-docs', swaggerDocs());
+  app.use(cookieParser());
+  app.use(router);
 
-  app.get('/contacts/:contactId', async (req, res) => {
+  app.use(notFoundHandler);
+  app.use(errorHandler);
 
-    const { contactId } = req.params;
-    if (!mongoose.Types.ObjectId.isValid(contactId)) {
-      return res.status(400).json({
-        message: 'Invalid contact ID format'
-      });
-    }
-    const contact = await getContactById(contactId);
-
-    if (!contact) {
-      return res.status(404).json({
-        message: 'Contact not found'
-      });
-    }
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  });
-
-  app.use((req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
   app.listen(PORT, (error) => {
     if (error) {
       throw error;
