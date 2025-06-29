ee490298dad2b0d36e46dee85b71804295673971
Tetyana Solyanyk <tanyas81@gmail.com>
2025-06-29 04:43:26 +0300

    Add functions for ingredients and category

--- src/services/category.js
+++ src/services/category.js
@@ -10,3 +10,25 @@ export const getAllCategories = async () => {
     throw createHttpError(500, `Failed to fetch categories - ${error.message}`);
   }
 };
+export const getCategoryById = async (id) => {
+  try {
+    const category = await Category.findById(id);
+    if (!category) {
+      throw createHttpError(404, 'Category not found');
+    }
+    return category;
+  } catch (error) {
+    throw createHttpError(500, `Failed to fetch category - ${error.message}`);
+  }
+};
+export const getCategoryByName = async (name) => {
+  try {
+    const category = await Category.findOne({ name });
+    if (!category) {
+      throw createHttpError(404, 'Category not found');
+    }
+    return category;
+  } catch (error) {
+    throw createHttpError(500, `Failed to fetch category - ${error.message}`);
+  }
+};
